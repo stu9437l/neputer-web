b244f6dac99c4da63e25b58e17a4a4b8017534cf
Sachin Rai <sachinkiranti@gmail.com>
2021-11-14 21:56:59 +0545

    fix: homepage setting issue

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -6,27 +6,32 @@
                 <div class="col-lg-6">
                     <div class="email-subs">
                         <h3>{{ $_settings['subscription_title'] ?? 'Enter Title for subscription' }}</h3>
-                        {!! $_settings['subscription_subtitle'] !!}
+                        {!! $_settings['subscription_subtitle'] ?? 'N/A' !!}
                     </div>
                 </div>
                 <div class="col-lg-6 v-center">
                     <div class="email-subs-form">
-                        <form action="{{route('subscribe.submit')}}" method="post" id="form-validation-subscribe">
+                        <form action="{{ route('subscribe.submit') }}" method="post" id="form-validation-subscribe">
                             @csrf
                             <input type="email" placeholder="Email Your Address" name="email" required>
-                            <button type="submit" name="submit" class="lnk btn-main bg-btn">{{ $_settings['subscription_button_name'] }} <i class="fas fa-chevron-right fa-icon"></i><span class="circle"></span></button>
+                            <button type="submit" name="submit" class="lnk btn-main bg-btn">{{ $_settings['subscription_button_name'] ?? 'N/A' }} <i class="fas fa-chevron-right fa-icon"></i><span class="circle"></span></button>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
     <div class="footer-row2">
         <div class="container">
             <div class="row justify-content-between">
                 <div class="col-lg-3 col-sm-6  ftr-brand-pp">
                     <a class="navbar-brand mb30 mt30" href="#">
-                        <img src="@if(isset($_settings['neputer_logo'])){{ \App\Facades\ViewHelperFacade::getImagePath('site_configuration',$_settings['neputer_logo']) }} @endif" alt="Neputer Logo" width="100">
+
+                        @isset($_settings['neputer_logo'])
+                            @php $logo = \App\Facades\ViewHelperFacade::getImagePath('site_configuration',$_settings['neputer_logo'] ?? 'N/A'); @endphp
+                        @endisset
+                        <img src="{{ $logo ?? 'n/a' }}" alt="Neputer Logo" width="100">
                     </a>
                  {!!  $_settings['footer_1'] ?? 'Footer 1' !!}
                 </div>
@@ -56,4 +61,5 @@
             </div>
         </div>
     </div>
+
 </footer>
