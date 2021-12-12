53191d9ffe9285a511259481ffc8d5278f961896
Rajesh Chaudhary <rajeshisnepali@gmail.com>
2021-12-12 22:32:25 +0545

    chore: add bulk updates

--- resources/views/frontend/home/Section/counter.blade.php
+++ resources/views/frontend/home/Section/counter.blade.php
@@ -5,7 +5,7 @@
             <div class="col-lg-4 col-sm-6">
                 <div class="statistics">
                     <div data-tilt data-tilt-max="20" data-tilt-speed="1000" class="statistics-img">
-                        <img src="@if(isset($_settings['icon1'])) {{ \App\Facades\ViewHelperFacade::getImagePath('site_configuration',$_settings['icon1']) }}@endif" alt="years" class="img-fluid lazy" />
+                        <img src="@if(isset($_settings['icon1'])) {{ \App\Facades\ViewHelperFacade::getImagePath('site_configuration',$_settings['icon1']) }}@endif" alt="milestones" class="img-fluid lazy" />
                     </div>
                     <div class="statnumb">
                         <span class="counter">{{ $_settings['counter_1'] ?? " Insert Counter 1 " }}</span><span>+</span>
@@ -16,7 +16,7 @@
             <div class="col-lg-4 col-sm-6">
                 <div class="statistics">
                     <div data-tilt data-tilt-max="20" data-tilt-speed="1000" class="statistics-img">
-                        <img src="@if(isset($_settings['icon2'])) {{ \App\Facades\ViewHelperFacade::getImagePath('site_configuration',$_settings['icon2']) }}@endif" alt="years" class="img-fluid lazy" />
+                        <img src="@if(isset($_settings['icon2'])) {{ \App\Facades\ViewHelperFacade::getImagePath('site_configuration',$_settings['icon2']) }}@endif" alt="milestones" class="img-fluid lazy" />
                     </div>
                     <div class="statnumb">
                         <span class="counter">{{ $_settings['counter_2'] ?? " Insert Counter 2 " }}</span><span>+</span>
@@ -29,7 +29,7 @@
             <div class="col-lg-3 col-sm-6">
                 <div class="statistics">
                     <div data-tilt data-tilt-max="20" data-tilt-speed="1000" class="statistics-img">
-                        <img src="@if(isset($_settings['icon3'])) {{ \App\Facades\ViewHelperFacade::getImagePath('site_configuration',$_settings['icon3']) }}@endif" alt="years" class="img-fluid lazy" />
+                        <img src="@if(isset($_settings['icon3'])) {{ \App\Facades\ViewHelperFacade::getImagePath('site_configuration',$_settings['icon3']) }}@endif" alt="milestones" class="img-fluid lazy" />
                     </div>
                     <div class="statnumb">
                         <span class="counter">{{ $_settings['counter_3'] ?? " Insert Counter 3 " }}</span><span>+</span>
@@ -40,7 +40,7 @@
             <div class="col-lg-3 col-sm-6">
                 <div class="statistics">
                     <div data-tilt data-tilt-max="20" data-tilt-speed="1000" class="statistics-img">
-                        <img src="@if(isset($_settings['icon4'])) {{ \App\Facades\ViewHelperFacade::getImagePath('site_configuration',$_settings['icon4']) }}@endif" alt="years" class="img-fluid lazy" />
+                        <img src="@if(isset($_settings['icon4'])) {{ \App\Facades\ViewHelperFacade::getImagePath('site_configuration',$_settings['icon4']) }}@endif" alt="milestones" class="img-fluid lazy" />
                     </div>
                     <div class="statnumb counter-number">
                         <span class="counter">{{ $_settings['counter_4'] ?? " Insert Counter 4 " }}</span><span>k</span>
@@ -51,7 +51,7 @@
             <div class="col-lg-3 col-sm-6">
                 <div class="statistics">
                     <div data-tilt data-tilt-max="20" data-tilt-speed="1000" class="statistics-img">
-                        <img src="@if(isset($_settings['icon5'])) {{ \App\Facades\ViewHelperFacade::getImagePath('site_configuration',$_settings['icon5']) }}@endif" alt="years" class="img-fluid lazy" />
+                        <img src="@if(isset($_settings['icon5'])) {{ \App\Facades\ViewHelperFacade::getImagePath('site_configuration',$_settings['icon5']) }}@endif" alt="milestones" class="img-fluid lazy" />
                     </div>
                     <div class="statnumb">
                         <span class="counter">{{ $_settings['counter_5'] ?? " Insert Counter 5 " }}</span><span>k</span>
@@ -63,7 +63,7 @@
             <div class="col-lg-3 col-sm-6">
                 <div class="statistics mb0">
                     <div data-tilt data-tilt-max="20" data-tilt-speed="1000" class="statistics-img">
-                        <img src="@if(isset($_settings['icon6'])) {{ \App\Facades\ViewHelperFacade::getImagePath('site_configuration',$_settings['icon6']) }}@endif" alt="years" class="img-fluid lazy" />
+                        <img src="@if(isset($_settings['icon6'])) {{ \App\Facades\ViewHelperFacade::getImagePath('site_configuration',$_settings['icon6']) }}@endif" alt="milestones" class="img-fluid lazy" />
                     </div>
                     <div class="statnumb">
                         <span class="counter">{{ $_settings['counter_6'] ?? " Insert Counter 6 " }}</span><span>hrs</span>
