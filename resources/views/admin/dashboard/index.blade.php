51736ab9827573b7e9b22d5853b526eaa9909f6a
Sachin Rai <sachinkiranti@gmail.com>
2021-12-15 09:13:45 +0545

    chore: remove template intro

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -23,22 +23,6 @@
             <div class="row">
                 <div class="col-xs-12">
                     <!-- PAGE CONTENT BEGINS -->
-                    <div class="alert alert-block alert-success">
-                        <button type="button" class="close" data-dismiss="alert">
-                            <i class="icon-remove"></i>
-                        </button>
-
-                        <i class="icon-ok green"></i>
-
-                        Welcome to
-                        <strong class="green">
-                            Ace
-                            <small>(v1.2)</small>
-                        </strong>
-                        ,
-                        the lightweight, feature-rich and easy to use admin template.
-                    </div>
-
                     <div class="row">
                         <div class="col-sm-4">
                             <div class="widget-box">
