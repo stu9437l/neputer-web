edd40ca1ca67b5c7c07f8a25c123e35a5253c8bc
Rajesh Chaudhary <rajeshisnepali@gmail.com>
2022-02-16 22:29:20 +0545

    refactor: modify few frontend pages
    service page
    page detail page

--- resources/views/frontend/service/index.blade.php
+++ resources/views/frontend/service/index.blade.php
@@ -41,27 +41,6 @@
     </section>
     <!--End Service-->
 
-    <!--Start CTA-->
-    <section class="cta-area pad-tb">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-8">
-                    <div class="common-heading">
-                        <span>Let's work together</span>
-                        <h2>We Love to Listen to Your Requirements</h2>
-                        <a href="{{ url('contact-us') }}" class="btn-outline">Estimate Project <i class="fas fa-chevron-right fa-icon"></i></a>
-                        <p class="cta-call">Or call us now <a href="tel:{{$_settings['company_phone']}}"><i class="fas fa-phone-alt"></i> {{$_settings['company_phone']}}</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="shape shape-a1"><img src="{{ asset('Frontend/images/shape/shape-3.svg') }}" alt="shape"/></div>
-        <div class="shape shape-a2"><img src="{{ asset('Frontend/images/shape/shape-4.svg') }}" alt="shape"/></div>
-        <div class="shape shape-a3"><img src="{{ asset('Frontend/images/shape/shape-13.svg') }}" alt="shape"/></div>
-        <div class="shape shape-a4"><img src="{{ asset('Frontend/images/shape/shape-11.svg') }}" alt="shape"/></div>
-    </section>
-    <!--End CTA-->
-    <!--Start Footer-->
 
 @endsection
 
